e41775d01edf7f5f27bdda1e5b2d3832283ac11a
Artur <devilyoushy@gmail.com>
2019-04-24 20:25:43 +0100

    Doing important job

---
+++ NeverGonnaGiveYouUp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#define Never int
+#define Gonna main
+#define Give (
+#define You )
+#define Up {
+#define never std::
+#define gonna cout <<
+#define let "Never gonna make you cry\n"
+#define you "Never gonna say goodbye\n"
+#define down ;
+#define run "Never gonna tell a lie\n"
+#define around "and hurt you\n";
+#define andd system("pause");
+#define desert return 0;
+#define youu }
+
+Never Gonna Give You Up
+    never gonna let you down
+        never gonna run around
+            andd desert youu
